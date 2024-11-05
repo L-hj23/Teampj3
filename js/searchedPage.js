2751581f933a0376898616db2304f4ace6dfaead
geongeongeon <leegeonhee0204@gmail.com>
2024-11-05 17:14:34 +0900

    feat: main-page, searched-page / progress: detail-page

---
+++ js/searchedPage.js
@@ -0,0 +1,18 @@
+const clickSubject = (subject) => {
+    console.log(subject);
+    $('#part1').removeClass('d-none');
+    $('.subject').text(subject);
+}
+
+
+$(document).ready(function() {
+    let price = $('.price').text("70,000");
+
+    $('#showFree').change(function() {
+        if ($(this).prop('checked')) {
+            $('.price').text("무료");
+        } else {
+            $('.price').text("70,000");
+        }
+    });
+});

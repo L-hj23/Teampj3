1788df148343bd01f4d13e2af701534e5ee98a46
geongeongeon <leegeonhee0204@gmail.com>
2024-11-23 21:48:10 +0900

    마이페이지 프로필 사진 수정 폼 추가

--- main/js/main.js
+++ main/js/main.js
@@ -3,4 +3,16 @@ $(document).ready(function() {
         event.preventDefault();
         alert('평가하기');
     });
+
+    $('#btnChangeProfileImage').on('click', function() {
+        $('#changeProfileImageForm').removeClass('d-none');
+    });
+
+    $('#btnCancleEdit').on('click', function() {
+        $('#changeProfileImageForm').addClass('d-none');
+    });
+
+    $('#btnUpload').on('click', function() {
+        $('#inputFile').click();
+    });
 });
